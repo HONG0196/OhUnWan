29795b9eb469f8605c4d321b2b10b98c62bf9a6f
Hong <ghdcks0196@gmail.com>
2023-08-04 17:15:56 +0900

    UI짜는중

---
+++ OhUnWan/VM/HomeViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewModel.swift
+//  OhUnWan
+//
+//  Created by 양홍찬 on 2023/08/04.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    // 데이터 (모델) ⭐️⭐️⭐️
+    private var userEmail: String
+    
+    // Output
+    var userEmailString: String {
+        return userEmail
+    }
+    
+    init(userEmail: String) {
+        self.userEmail = userEmail
+    }
+    
+    // Logic...
+    
+}
